89697fb25763964562ddbf0183a9c9a00a57c15c
Adnan Rahman <adnanibnamotin@yahoo.com>
2019-12-30 01:31:00 -0500

    implement nav bar

---
+++ src/components/nav/NavBar.svelte
@@ -0,0 +1,36 @@
+<script>
+  import {
+    Container,
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+</script>
+
+<Navbar color="primary" dark expand="md">
+<Container>
+  <NavbarBrand href="/">Budget Calculator</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ml-auto" navbar>
+      <NavItem>
+        <NavLink href="#components/">[ + ] Add item</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Container>
+</Navbar>
