1669d4121552b6a375d715f483498a710e0c3296
Adnan Rahman <adnanibnamotin@yahoo.com>
2020-01-02 22:07:51 -0500

    Add title component

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,22 @@
 <script>
 	import { Col, Container, Row, Button } from "sveltestrap";
 	import NavBar from './components/nav/NavBar.svelte';
+	import Title from "./components/title/Title.svelte";
 	const colors = [
     "primary", "secondary", "success", "danger", "warning", "info", "light", "dark"
   ];
 	export let name;
 </script>
 
-<main>
+<div>
 	<NavBar />
 	<Container>
-	<h1>Hello {name}!</h1>
+	<Title />
 	<Button color="success">
 		Button
 	</Button>
 	</Container>
-</main>
+</div>
 <style>
 
 </style>
---
+++ src/components/title/Title.svelte
@@ -0,0 +1,7 @@
+<script>
+export let title = "Default Title";
+</script>
+
+<div>
+<h1>{ title }</h1>
+</div>
